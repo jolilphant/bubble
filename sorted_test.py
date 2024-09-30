5a8dfbe8982cd4653f2bb44c6a508aa8441a40d7
jolilphant <102636013+jolilphant@users.noreply.github.com>
2024-09-30 13:21:46 -0600

    Update sorted_test.py

--- sorted_test.py
+++ sorted_test.py
@@ -3,5 +3,11 @@ import random
 
 nums = [random.randint(1,100) for x in range(random.randint(1,100))]
 
-def test_sorted():
+def test_bubble():
+  assert sorting.bubble(nums) == sorted(nums)
+
+def test_selection():
+  assert sorting.bubble(nums) == sorted(nums)
+
+def test_insertion():
   assert sorting.bubble(nums) == sorted(nums)
