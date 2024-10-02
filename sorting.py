5a603aa2b81dc1e5c9c08f308b7be360b93cf717
jolilphant <102636013+jolilphant@users.noreply.github.com>
2024-10-02 11:36:49 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,5 +1,9 @@
-#bubble(list) - > sorted ist
+#bubble(list) -> sorted ist
 
 #selection(list) -> sorted list
 
-#insertion(list) > sorted list
+#insertion(list) -> sorted list
+
+#shell(list) -> sorted list
+
+#quicksort(list) -> sorted list
